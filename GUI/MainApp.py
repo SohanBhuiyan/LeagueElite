8469aa71aaf7968ec7fa0756166b1539cc8692bd
Sohan Bhuiyan <sohanura@gmail.com>
2019-06-01 21:57:25 -0400

    Basic GUI window

---
+++ GUI/MainApp.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+# label1 = Label(root, text = "some text", bg = "red", fg = "white")
+# label2 = Label(root, text = "some text2", bg = "yellow", fg = "white")
+# label3 = Label(root, text = "some text3", bg = "purple", fg = "white")
+# label1.pack()
+# label2.pack(fill=X)
+# label3.pack(side=LEFT,fill=Y)
+root.mainloop()

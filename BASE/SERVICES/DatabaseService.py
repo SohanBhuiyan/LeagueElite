ab31f2e6a0fdc8b32a6ee575c88c558c1f556714
Sohan Bhuiyan <sohanura@gmail.com>
2019-06-03 19:40:56 -0400

    Added two exception handling to populateWithPlayerJSON

--- BASE/SERVICES/DatabaseService.py
+++ BASE/SERVICES/DatabaseService.py
@@ -1,4 +1,4 @@
-from BASE.DATABASE.Main import DatabaseService
+from BASE.DATABASE.Main import Database
 import json
 import os
 
@@ -6,24 +6,42 @@ import os
 
 class DatabaseService:
 
-    databaseService = DatabaseService()
+    databaseService = Database()
 
+
+    # The file should have .json extension.
     def populateWithPlayerJSON(self,jsonFile, tier):
-        curr_dir = os.path.abspath(os.path.join(__file__, os.pardir))  # able to get parent directory of current module
-        rel_path = "JSON/"+jsonFile
-        abs_file_path = os.path.join(curr_dir, rel_path)
-        with open(abs_file_path, encoding='utf-8') as data_file:
-            data = json.loads(data_file.read())
+        try:
+            # curr_dir, rel_path, abs_file_path are just file path setup to find the JSON file.
+            curr_dir = os.getcwd()
+            rel_path = "JSON/"+jsonFile
+            abs_file_path = os.path.join(curr_dir, rel_path) # eg. BASE/JSON/Challenger.jso
+            with open(abs_file_path, encoding='utf-8') as data_file:
+                data = json.loads(data_file.read())
             for entry in data["entries"]:
                 summonername = entry["summonerName"]
                 rank = tier
                 self.databaseService.insertIntoProfile(summonername,rank)
-        print("Populated with players in tier:{}".format(tier))
+            print("Populated with players in tier:{}".format(tier))
+        except FileNotFoundError:
+            print("ERROR: File name does not exist inside of JSON dir")
+        except TypeError:
+            print("ERROR: jsonFile format is incorrect. View RIOT's API Response Body for correct format")
 
 
-        #todo: Need to work on a method which populates teh champion_mastery table with each pro players mastery.
 
 
-    def populateChampionMastery(self):
-        None
+        #todo: Need to work on a method which populates the champion_mastery table with each pro players mastery.
 
+    def populateMasteryTable(self):
+        cursor = self.databaseService.select("* FROM profiles")
+        for data in cursor:
+            print(data)
+
+    def getID(self, summonername):
+        self.databaseService.select("From profiles WHERE summonername = {}".format(summonername))
+
+    def find(name, path):
+        for root, dirs, files in os.walk(path):
+            if name in files:
+                return os.path.join(root, name)

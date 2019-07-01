3d31961270da938f6f548f00008427a1b5297abd
Sohan Bhuiyan <sohanura@gmail.com>
2019-07-01 18:03:09 -0400

    * removed method that wasnt used anymore * changed create_player_json to update_rosters for a fitting name

--- BASE/SERVICES/LeagueService.py
+++ BASE/SERVICES/LeagueService.py
@@ -1,34 +1,14 @@
-from urllib.request import urlopen
-from BASE.SERVICES.API import get_challenger_players
-from BASE.SERVICES.API import get_grandmaster_players
-from BASE.SERVICES.API import get_master_players
+from BASE.SERVICES.API import API
 from BASE.DATABASE.Database import Database
 import json
 
 import os
 
-
-
-def find_pro_player():
-    #if champ mastery is the highest, then return it
-    # parse each tier JSON file, challenger, gm, master and try to find the highest player
-
-    highest_mastery = None
-    highest_mastery_user = None
-
-    challenger_json = get_challenger_players()
-
-    for entry in challenger_json["entries"]:
-        if entry["wins"] > 350:
-            print("Jesus {}, u got {} wins"
-                  .format(entry["summonerName"],
-                          entry["wins"]))
-
-def create_player_json():
-
-    challenger_json = get_challenger_players()
-    grandmaster_json = get_grandmaster_players()
-    master_json = get_master_players()
+API = API()
+def update_rosters():
+    challenger_json = API.get_challenger_players()
+    grandmaster_json = API.get_grandmaster_players()
+    master_json = API.get_master_players()
 
     #return dir of the file that called this method, not nessecarily the directory this file is under.
     dir = os.getcwd()
@@ -47,13 +27,7 @@ def create_player_json():
         json.dump(master_json, outfile)
 
 
-"""
-@param: 
-jsonData: JSON data of different tiered players
-champId: the id that is associated with a champion name. Method in user class can convert champname to id
-"""
 def get_highest_mastery_entry_for(champId):
     db = Database()
-    max_entry = db.select("SELECT * FROM champion_mastery WHERE score=(SELECT MAX(score) FROM champion_mastery WHERE champId = {})".format(champId))
-    return max_entry
-
+    max_entry = db.select(" * FROM champion_mastery WHERE score=(SELECT MAX(score) FROM champion_mastery WHERE champId = {})".format(champId))
+    return max_entry[0] # the first and only element in the list

4db0b862b68e3358d4bd991334ecd17a3ecc3892
Sohan Bhuiyan <sohanura@gmail.com>
2019-07-15 14:12:35 -0400

    Finish update_mastery_table
    * The function now correctly populates the mastery table
    * added api_limiter to avoid too many requests

--- BASE/SERVICES/DatabaseService.py
+++ BASE/SERVICES/DatabaseService.py
@@ -1,13 +1,14 @@
-import os
 import time
 
 from BASE.DATABASE.Database import Database
-from BASE.SERVICES.API import get_highest_champion_mastery_by_summonerID
+from BASE.SERVICES.API import API
 from BASE.Utility import *
 
 
 class DatabaseService:
+    CHAMP_ID_INDEX = 2
     ID_INDEX = 0
+    SUMMONER_INDEX = 1
     database = Database()
     def insert_players_by_rank(self, rank):
         try:
@@ -41,27 +42,38 @@ class DatabaseService:
 
     def update_mastery_table(self):
         api_call_count = 0
+        apiHelper = API()
         print("Please wait one moment...")
         profile_list = self.database.select("* FROM profiles")
         for profile in profile_list:
-
-            if api_call_count == 100: #my api limit is 200 per 2min, so we will wait it out
+            # API limitation condition
+            if api_call_count == 99: #my api limit is 100 per 2min, so we will wait it out
+                print("almost at api limit call, waiting 3 min...")
                 time.sleep(180)
                 api_call_count = 0 # reset api_call_count after we waited about two minutes
+                print("finish waiting, continuing process")
 
-            summonerID = profile[self.ID_INDEX]
-            summonerName = profile[1]
+            profileID = profile[self.ID_INDEX] # needed to call certain API methods
+            summonerName = profile[self.SUMMONER_INDEX] # what the client sees.
 
-            highest_mastery_entry = get_highest_champion_mastery_by_summonerID(summonerID)
+            highest_mastery_entry = apiHelper.get_highest_champion_mastery_by_summonerID(profileID)
             api_call_count = api_call_count + 1
+
             mastery_points = highest_mastery_entry["championPoints"]
             champion_id = highest_mastery_entry["championId"]
-            self.database.insert_into_champion_mastery_table(summonerName,mastery_points,champion_id)
-            time.sleep(.1) # to prevent overdoing api requests in a short period of time
+
+            #check if the player already exist in the db. If so, just update its values
+            possible_duplicate = self.database.select("* FROM champion_mastery WHERE summonername=\"{}\"".format(summonerName))
+
+            #if a duplicate exist, same name and champID, we just update score
+            if possible_duplicate:
+                if possible_duplicate[0][self.CHAMP_ID_INDEX] == champion_id:
+                    self.database.update("champion_mastery","score",mastery_points,summonerName)
+            else:
+                self.database.insert_into_champion_mastery_table(summonerName,mastery_points,champion_id)
+            time.sleep(.05) # to prevent overdoing api requests in a short period of time
         print("Done populating champion_mastery table")
 
 
     def getID(self, summonername):
         self.database.select("From profiles WHERE summonername = {}".format(summonername))
-
-
